56ba8111fc016191619b77928ee2208b5ce14c5e
CStobs <chelsea.stobbart@gmail.com>
2014-09-12 16:11:02 -0400

    Adding footer and header

---
+++ footer.js
@@ -0,0 +1,17 @@
+document.write('\
+\
+    <footer id="footer">\
+        <ul class="actions">\
+            <li><a href="https://twitter.com/clothesie" class="fa solo fa-twitter"><span>Twitter</span></a></li>\
+            <li><a href="https://www.facebook.com/clothesie" class="fa solo fa-facebook"><span>Facebook</span></a></li>\
+            <li><a href="http://instagram.com/clothesie" class="fa solo fa-instagram"><span>Instagram</span></a></li>\
+        </ul>\
+\
+        <!-- Menu -->\
+        <ul class="menu">\
+            <li>&copy; </li>\
+            <li>Website created by Chelsea Stobbart</a></li>\
+        </ul>\
+    </footer>\
+\
+');
---
+++ header.js
@@ -0,0 +1,20 @@
+document.write('\
+\
+    <header id="header">\
+        <!-- Logo -->\
+        <div id="logo"><a href="#"><img src="images/logo.png"/></a></div>\
+\
+        <!-- Nav -->\
+        <nav id="nav">\
+            <ul>\
+                <li><a href="#intro">Home</a></li>\
+                <li><a href="#shop">About</a></li>\
+                <li><a href="#sell">Sell</a></li>\
+                <li><a href="#login">Login</a></li>\
+                <li><a href="#help">Help</a></li>\
+            </ul>\
+        </nav>\
+\
+    </header>\
+\
+');
